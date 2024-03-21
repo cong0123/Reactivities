148948239888ff1de737a65342b3aa9279d8b593
cong0123 <shou.usc@gmail.com>
2024-03-21 23:41:16 +0700

    End of section 14

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,4 +1,6 @@
 using Application.Core;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Domain;
 using MediatR;
 using Microsoft.AspNetCore.Http.HttpResults;
@@ -10,22 +12,25 @@ namespace Application.Activities
 {
     public class List
     {
-        public class Query : IRequest<Result<List<Activity>>> { }
+        public class Query : IRequest<Result<List<ActivityDto>>> { }
 
-        public class Handler : IRequestHandler<Query, Result<List<Activity>>>
+        public class Handler : IRequestHandler<Query, Result<List<ActivityDto>>>
         {
             private readonly DataContext _context;
-            private readonly ILogger<List> _logger;
+            private readonly IMapper _mapper;
 
-            public Handler(DataContext context, ILogger<List> logger)
+            public Handler(DataContext context, IMapper mapper)
             {
                 _context = context;
-                _logger = logger;
+                _mapper = mapper;
             }
 
-            public async Task<Result<List<Activity>>> Handle(Query request, CancellationToken token)
+            public async Task<Result<List<ActivityDto>>> Handle(Query request, CancellationToken token)
             {
-                return Result<List<Activity>>.Success(await _context.Activities.ToListAsync(token));
+                var activities = await _context.Activities
+                  .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider)
+                .ToListAsync(token);
+                return Result<List<ActivityDto>>.Success(activities);
             }
         }
     }
